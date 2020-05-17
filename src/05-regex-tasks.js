45f45b7f14edd8cd3f48e9d64835b88971dc02ef
Volkau Aliaksey <bobrui4anin@gmail.com>
2020-05-17 16:07:21 +0300

    feat: add solution for getPasswordValidator

--- src/05-regex-tasks.js
+++ src/05-regex-tasks.js
@@ -78,8 +78,9 @@ function getRegexForPitSpot() {
  *   'PASSW0RD'.match(validator)  => false
  *   'Pa55'.match(validator) => false
  */
-function getPasswordValidator(/* minLength */) {
-  throw new Error('Not implemented');
+function getPasswordValidator(minLength) {
+  const reg = `(?!.*[\\s])(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[a-zA-Z^0-9]{${minLength},}`;
+  return new RegExp(reg);
 }
 
 
